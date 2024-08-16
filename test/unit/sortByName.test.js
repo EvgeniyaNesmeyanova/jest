dd23d9c2f61893b0c9a77e68a1d31e4937a18954
EvgeniyaNesmeyanova <prof_nea@mail.ru>
2024-08-16 15:51:21 +0500

    add project

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,31 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+       "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+});
+
+describe("The same book titles", () => {
+  it("Books names do not require sorting", () => {
+    expect(
+      sorting.sortByName([
+      "Гарри Поттер",
+      "Гарри Поттер",
+      ])
+    ).toEqual([
+    "Гарри Поттер",
+     "Гарри Поттер", 
+    ]);
+  });
+});
